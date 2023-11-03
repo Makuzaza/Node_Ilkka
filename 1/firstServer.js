7d15fcc2d2c0913b5132d60cea799884a9c59e5b
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-11-03 15:11:34 +0200

    second week

--- 1/firstServer.js
+++ 1/firstServer.js
@@ -6,6 +6,7 @@ const port = 3000;
 const host = 'localhost'; //'127.0.0.1'
 
 const server = http.createServer((request, response)=>{
+    console.log('function called')
     response.writeHead(200,{
         'Content-Type':'text/plain;charset=utf-8'
     });
---
+++ 2/1_requestDemo.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const http = require('http');
+
+const {port, host} = require('./config.json');
+
+const server = http.createServer((req,res)=>{
+    // console.log(req.url);
+    const urldata=new URL(`http://${host}:${port}${req.url}`);
+    console.log(urldata);
+    const {pathname}=urldata;
+    res.writeHead(200,{
+        'Content-Type':'text/plain'
+    });
+    if(pathname==='/aaa') {
+        res.write('aaa');
+    } else {
+        res.write('xyz')
+    }
+    res.end();
+});
+
+server.listen(port, host, 
+    ()=>console.log(`${host}:${port} serving...`));
